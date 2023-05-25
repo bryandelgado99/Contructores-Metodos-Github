721cc3c7fc3bd11c52e051e002ff8c34f25fda68
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-05-24 23:37:36 -0500

    Creacion de atributos y constructores

--- Sistema Peliculas - Delgado_Espinel_Sangucho/src/Peliculas.java
+++ Sistema Peliculas - Delgado_Espinel_Sangucho/src/Peliculas.java
@@ -2,6 +2,26 @@
    podemos mantener un control más limpio de los cambios en los archivos Java*/
 public class Peliculas {
     //Atributos
+    String nombre, director, actor_principal, productora, genero, pais, taquilla;
+    int anio;
+
+    //Constructores
+    public Peliculas(String nombre, String director, String actor_principal, String productora, String genero, String pais, int anio, String taquilla) {
+        this.nombre = nombre;
+        this.director = director;
+        this.actor_principal = actor_principal;
+        this.productora = productora;
+        this.genero = genero;
+        this.pais = pais;
+        this.anio = anio;
+        this.taquilla = taquilla;
+    }
+
 
     //Métodos
+
 }
+
+/*public void imprimir_Datos(){
+
+}*/
