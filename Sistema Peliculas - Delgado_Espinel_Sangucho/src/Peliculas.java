7e2a937baf04086bb8b3ee6b95c7a3ab8f2bf38c
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-05-24 23:10:33 -0500

    Update Peliculas.java

--- Sistema Peliculas - Delgado_Espinel_Sangucho/src/Peliculas.java
+++ Sistema Peliculas - Delgado_Espinel_Sangucho/src/Peliculas.java
@@ -1,3 +1,5 @@
+/*Hola compañeras, les recomiendo que por favor coloquen un comentario con su nombre hasta la parte que agregaron, asi
+   podemos mantener un control más limpio de los cambios en los archivos Java*/
 public class Peliculas {
     //Atributos
 
