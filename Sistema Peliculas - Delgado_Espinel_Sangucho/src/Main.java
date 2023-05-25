c453edc714ea434d4dcb345d076688d6622572c3
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-05-24 23:37:59 -0500

    Creacion de instancias film1, film2 y film3

--- Sistema Peliculas - Delgado_Espinel_Sangucho/src/Main.java
+++ Sistema Peliculas - Delgado_Espinel_Sangucho/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        //Creacion de isntancias
+        Peliculas film1 = new Peliculas("Gigolo Por Accidente", "Mike Mitchell",
+                "Rob Schneider", "Happy Madison", "Comedia",
+                "Estados Unidos", 1999, "$92 millones");
+        Peliculas film2 = new Peliculas("Django Desencadenado", "Quentin Tarantino",
+                "Jamie Foxx", "Columbia Pictures", "Wester Drama",
+                "Estados Unidos", 2012, "$425 millones");
+        Peliculas film3 = new Peliculas("Rtas, ratones y rateros", "Sebastian Cordero",
+                "Carlos Valencia, Marco Bustos, Simon Braurer", "Cabezahueca",
+                "Drama - Crimen", "Ecuador", 1999, "$41 mil");
 
+
+        //Menu Inicial
+        System.out.println("---------- Bienvenido a PeliMaster! ---------");
     }
 }
